9d52909e6831dd992a63327facb1e2c05d1d4b67
Mitsubi <agp.842007@gmail.com>
2026-08-26 11:27:36 +0200

    i made every page having the same side bar menu

---
+++ app/contact.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact page</title>
+    <!--boxicons cdn links--->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="settings.css">
+</head>
+
+<body>
+    <div class="sidebar">
+        <i class='bx bx-menu' id='btn'></i>
+        <div class="logo-content">
+            <div class="logo">
+                <div class="logo-name">Mitsu</div>
+
+            </div>
+
+
+        </div>
+        <ul class="navList">
+            <li>
+                <i class='bx bx-search'></i>
+                <input type="text" placeholder="search...">
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-home'></i>
+                    <span class="links_name">Home</span>
+                </a>
+                <span class="tooltip">Home</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-user'></i>
+                    <span class="links_name">About me</span>
+                </a>
+                <span class="tooltip">About me</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-photo-album'></i>
+                    <span class="links_name">gallery</span>
+                </a>
+                <span class="tooltip">gallery</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-joystick'></i>
+                    <span class="links_name">Games</span>
+                </a>
+                <span class="tooltip">Games</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-check-square'></i>
+                    <span class="links_name">Goals</span>
+                </a>
+                <span class="tooltip">Goals</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bxs-folder'></i>
+                    <span class="links_name">Other stuff</span>
+                </a>
+                <span class="tooltip">Other stuff</span>
+            </li>
+        </ul>
+    </div>
+    <div class="home_content">
+        <div class="text">Home Page</div>
+    </div>
+    <script src="javascript.js"></script>
+</body>
+
+</html>
