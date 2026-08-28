fdab5f78907800df3d0a7f30659c0a1f9800630e
Mitsubi <agp.842007@gmail.com>
2026-08-28 16:14:11 +0200

    removed the three pictures from files, Added for every page the same layout--> sidebar,topbar,footer and background. added an animated background that makes flowing cherry blossom petals flowing out the background, fixed label, added icons for clickable links for my social links.

--- app/games.php
+++ app/games.php
@@ -15,72 +15,93 @@
         <i class='bx bx-menu' id='btn'></i>
         <div class="logo-content">
             <div class="logo">
-                <div class="logo-name">Mitsu</div>
+                <div class="logo-name">MITSU</div>
 
             </div>
 
 
         </div>
-        <ul class="navList">
+      <ul class="navList">
             <li>
                 <i class='bx bx-search'></i>
                 <input type="text" placeholder="search...">
             </li>
             <li>
-                <a href="#">
+                <a href="homepage.php">
                     <i class='bx bxs-home'></i>
                     <span class="links_name">Home</span>
                 </a>
                 <span class="tooltip">Home</span>
             </li>
             <li>
-                <a href="#">
+                <a href="aboutpage.php">
                     <i class='bx bxs-user'></i>
                     <span class="links_name">About me</span>
                 </a>
                 <span class="tooltip">About me</span>
             </li>
             <li>
-                <a href="#">
+                <a href="gallerypage.php">
                     <i class='bx bxs-photo-album'></i>
                     <span class="links_name">gallery</span>
                 </a>
                 <span class="tooltip">gallery</span>
             </li>
             <li>
-                <a href="#">
+                <a href="games.php">
                     <i class='bx bxs-joystick'></i>
                     <span class="links_name">Games</span>
                 </a>
                 <span class="tooltip">Games</span>
             </li>
             <li>
-                <a href="#">
+                <a href="goals.php">
                     <i class='bx bxs-check-square'></i>
                     <span class="links_name">Goals</span>
                 </a>
                 <span class="tooltip">Goals</span>
             </li>
             <li>
-                <a href="#">
+                <a href="otherstuff.php">
                     <i class='bx bxs-folder'></i>
                     <span class="links_name">Other stuff</span>
                 </a>
                 <span class="tooltip">Other stuff</span>
             </li>
         </ul>
-        <!-- SOCIAL BOXES: replace the image paths and # links with your social media details. -->
-        <div class="social-boxes">
-            <a class="social-box" href="#"><img src="afbeeldingen/social-1.png" alt="Social media 1"></a>
-            <a class="social-box" href="#"><img src="afbeeldingen/social-2.png" alt="Social media 2"></a>
-            <a class="social-box" href="#"><img src="afbeeldingen/social-3.png" alt="Social media 3"></a>
-            <a class="social-box" href="#"><img src="afbeeldingen/social-4.png" alt="Social media 4"></a>
+        <!-- SOCIAL LINKS: replace the # values with your social media URLs. -->
+         <div class="social-boxes">
+            <a class="social-box" href="#" aria-label="Social link A"><i class='bx bxl-discord-alt' ></i></a>
+            <a class="social-box" href="#" aria-label="Social link B"><i class='bx bxl-youtube' ></i></a>
+            <a class="social-box" href="#" aria-label="Social link C"><i class='bx bxl-tiktok' ></i></a>
+            <a class="social-box" href="#" aria-label="Social link D"><i class='bx bxl-github' ></i></a>
+            
         </div>
     </div>
     <div class="home_content">
-        <div class="text">Home Page</div>
+        <div class="top_bar">
+        </div>
+        <div class="background-picture">
+            <div class="cherry-blossom" aria-hidden="true"></div>
+            <div class="block-text1">
+                <p>GAMES</p>
+            </div>
+        </div>
+        <div class="down_bar"></div>
     </div>
+
+    </div>
+
+    <div class="placeholder-space">
+    </div>
+
+
+
     <script src="javascript.js"></script>
+
+    <div class="footer">
+<h1>Made with html, css, php and JS</h1>
+    </div>
 </body>
 
 </html>
--- app/javascript.js
+++ app/javascript.js
@@ -1,11 +1,32 @@
 const menuButton = document.querySelector("#btn");
 const sidebar = document.querySelector(".sidebar");
 const searchButton = document.querySelector(".bx-search");
+const dropbarButton = document.querySelector(".dropbar_button");
 
-menuButton.onclick = function () {
-    sidebar.classList.toggle("active");
-};
+const blossom = document.querySelector(".cherry-blossom");
 
-searchButton.onclick = function () {
-    sidebar.classList.toggle("active");
-};
+if (blossom) {
+    for (let index = 0; index < 40; index += 1) {
+        const petal = document.createElement("span");
+        petal.className = "petal";
+        petal.style.setProperty("--petal-x", `${Math.random() * 100}%`);
+        petal.style.setProperty("--petal-delay", `${Math.random() * -12}s`);
+        petal.style.setProperty("--petal-duration", `${7 + Math.random() * 5}s`);
+        petal.style.setProperty("--petal-drift", `${40 + Math.random() * 100}px`);
+        petal.style.setProperty("--petal-size", `${7 + Math.random() * 7}px`);
+        blossom.appendChild(petal);
+    }
+}
+
+if (menuButton && sidebar) {
+    menuButton.onclick = function () {
+        sidebar.classList.toggle("active");
+    };
+}
+
+if (searchButton && sidebar) {
+    searchButton.onclick = function () {
+        sidebar.classList.toggle("active");
+    };
+}
+
